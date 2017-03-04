9097dfe22df7e1968c236f53b8ee03d47f0bdeae
dlanier <lanier4@illinois.edu>
2017-03-04 11:47:53 -0600

    frame points with demo/doc completed

--- src/z_plane.py
+++ src/z_plane.py
@@ -1,45 +1,5 @@
 """
 complex plane functions and class for getting pixels in three different measuring systems
-
-
-        function framePoints = getFrame(h, w, theta, ZM, CP)
-            %Usage: framePoints = obj.getFramePoints;
-            %struct of vectors that define the pixels on the complex plane
-
-            % scale and rotate two vectors to the object "zoom" factor
-            if w >= h
-                framePoints.top_center = exp(1i * (pi / 2 + theta)) / ZM;
-                framePoints.right = (w/h) * exp(1i * theta) / ZM;
-            else
-                framePoints.top_center = (h/w) * exp(1i *(pi/2 + theta))/ZM;
-                framePoints.right = exp(1i * theta) / ZM;
-            end
-
-            % vector addition == other frame point vectors
-            framePoints.bottom_center = -1 * framePoints.top_center;
-            framePoints.left = -1 * framePoints.right;
-            framePoints.upper_right = framePoints.right +...
-                                        framePoints.top_center;
-            framePoints.lower_right = framePoints.right +...
-                                        framePoints.bottom_center;
-            framePoints.upper_left = framePoints.left +...
-                                        framePoints.top_center;
-            framePoints.lower_left = framePoints.bottom_center -...
-                                        framePoints.right;
-
-            % (struct) framePoints: shift all to start at the center point
-            nms = fieldnames(framePoints);
-            for k = 1:numel(nms)
-                framePoints.(nms{k}) = framePoints.(nms{k}) + CP;
-            end
-        end
-
-
-    upper_left,      top_center,      upper_right
-
-    left_center,     center_point,    right_center
-
-    bottom_left,     bottom_center,   bottom_right
 """
 import numpy as np
 
@@ -47,23 +7,6 @@ print_order = ['upper_left', 'top_center', 'upper_right',
                'left_center', 'center_point', 'right_center',
               'bottom_left', 'bottom_center', 'bottom_right']
 
-
-def get_complex_frame_string(fd):
-    """ get a formatted list of strings """
-    frame_string = ''
-    row = 0
-    for k in print_order:
-        r = np.real(fd[k])
-        i = np.imag(fd[k])
-        row += 1
-        if np.mod(row,3) == 0:
-            frame_string = frame_string + '\t %s \t %0.9f %0.9f \n'%(k,r,i)
-        elif np.mod(row,3) == 1:
-            frame_string = frame_string + '%s \t %0.9f %0.9f' % (k, r, i)
-        else:
-            frame_string = frame_string + '\t %s \t %0.9f %0.9f' % (k, r, i)
-
-    return frame_string
 
 def get_complex_frame(CP, ZM, theta, h=1, w=1):
     """ get the complex numbers at ends and centers of a frame  """
@@ -83,13 +26,66 @@ def get_complex_frame(CP, ZM, theta, h=1, w=1):
     frame_dict['upper_left'] = frame_dict['left_center'] + frame_dict['top_center']
     frame_dict['bottom_left'] = frame_dict['left_center'] + frame_dict['bottom_center']
 
+    for k in frame_dict.keys():
+        frame_dict[k] = frame_dict[k] + CP
+
     return frame_dict
 
 
+def complex_to_string(z, N_DEC=6):
+    """ format single complex number to string with n decimal places """
+    MAX_DEC = 17
+    MIN_DEC = 1
+
+    n = max(min(MAX_DEC, round(N_DEC)), MIN_DEC)
+    fs = '%%0.%df'%n
+
+    zr = np.real(z)
+    zi = np.imag(z)
+
+    if np.sign(zi) < 0:
+        s1 = ' ' + fs % zi + 'j'
+    else:
+        s1 = ' +' + fs % zi + 'j'
+
+    if np.sign(zr) < 0:
+        z_str = fs % zr + s1
+    else:
+        z_str = ' ' + fs % zr + s1
+
+    return z_str
 
 
+def get_complex_frame_string(frame_dict, N_DEC=4):
+    """ get a formatted list of strings """
+
+    Z_INDENT = 14
+    CELL_SPC = 3
+
+    frame_string = []
+    row = 0
+    for k in print_order:
+        z_str = complex_to_string(frame_dict[k], N_DEC)
+        spc1 = ' ' * (Z_INDENT - len(k))
+        row += 1
+        if np.mod(row, 3) == 1:
+            row_string = [k + spc1 + z_str]
+        elif np.mod(row, 3) == 0:
+            spc0 = ' ' * CELL_SPC
+            row_string.append(spc0 + k + spc1 + z_str)
+            frame_string.append(row_string)
+        else:
+            spc0 = ' ' * CELL_SPC
+            row_string.append(spc0 + k + spc1 + z_str)
+
+    return frame_string
 
 
+def show_complex_frame(frame_dict):
+    """ display a frame_dict in 3 x 3 table format """
+    frame_string = get_complex_frame_string(frame_dict)
+    for l in frame_string:
+        print(l[0], l[1], l[2])
 
 
 
