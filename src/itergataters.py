af66900aca3e306e1b1ac7244044904e3f491c22
dlanier <lanier4@illinois.edu>
2017-03-26 12:44:06 -0500

    parallel iterator initial commit

---
+++ src/itergataters.py
@@ -0,0 +1,245 @@
+# -*- coding: utf-8 -*-
+"""
+@author: del
+lanier4@illinois.edu
+mradmstr514226508@gmail.com
+"""
+import os
+# import sys
+import time
+import itertools
+import multiprocessing as mp
+
+import numpy as np
+
+# sys.path.append(1, './')
+import z_plane as zp
+
+
+def get_primitives(list_tuple, par_set, delete_temp_dir=True):
+    """ Usage: ET, Z, Z0 = get_primitives(list_tuple, par_set) """
+    par_set['tmp_dir'] = get_tmp_dir(par_set['dir_path'], 'tmp')
+    complex_frame, par_set = zp.get_frame_from_dict(par_set)
+    n_cores = mp.cpu_count()
+    range_enumeration = np.int_(range(0, par_set['n_rows']))
+    core_pool = mp.Pool(processes=n_cores)
+    core_pool.starmap(write_row, 
+                     zip(itertools.repeat(complex_frame),
+                         itertools.repeat(list_tuple),
+                         itertools.repeat(par_set),
+                         range_enumeration))
+    core_pool.close()
+    core_pool.join()
+
+    Z0, Z, ET = assemble_rows(par_set)
+    
+    if delete_temp_dir: remove_tmp_dir(par_set['tmp_dir'])
+
+    return ET, Z, Z0
+
+
+def assemble_rows(par_set):
+    """ parallel large assembly step """
+    n_rows = par_set['n_rows']
+    n_cols = par_set['n_cols']
+    tmp_dir = par_set['tmp_dir']
+    
+    Z0 = np.zeros((n_rows, n_cols),complex)
+    Z = np.zeros((n_rows, n_cols),complex)
+    ET = np.zeros((n_rows, n_cols))
+    
+    dir_listing = os.listdir(tmp_dir)
+    for f in dir_listing:
+        fnm = os.path.join(tmp_dir, f)
+        three_sum = np.load(fnm)
+        row_number = int(np.imag(three_sum[1, 0]))
+        Z0[row_number, :] = three_sum[0, :]
+        ET[row_number, :] = np.real(three_sum[1, :])
+        Z[row_number, :] = three_sum[2, :]
+
+    return Z0, Z, ET
+
+
+def werker_bee(fcn_hndl, complex_frame, par_set, row_number):
+    """  parallel branch  """
+    left_style = np.linspace(complex_frame['upper_left'], complex_frame['bottom_left'], par_set['n_rows'])
+    right_style = np.linspace(complex_frame['upper_right'], complex_frame['bottom_right'], par_set['n_rows'])
+    z_row = np.array(np.zeros((1, par_set['n_cols'])), dtype=complex)
+    z_row[0, :] = np.linspace(left_style[row_number], right_style[row_number], par_set['n_cols'])
+
+    processes_row(z_row[0], row_number, fcn_hndl, par_set)
+    return
+
+def processes_row(row_array, row_number, fcn_hndl, par_set):
+    """ tricky row processing proceedure for parallel processing large slow graphic """
+    Z_arr = np.zeros((3,row_array.size),complex)
+    p = par_set['para_meter']
+    it_max = par_set['it_max']
+    max_d = par_set['max_d']
+    
+    col = 0
+    for Z0 in row_array:
+        Z_arr[0, col] = Z0
+        if p is 'use_Z0': p = Z0
+        Z_arr[1, col], Z_arr[2, col] = fcn_hndl(Z0, it_max, max_d, p)
+        col += 1
+        
+    Z_arr[1, :] = Z_arr[1, :] + complex(0.0, float(row_number)) # save row number in complex part of ET
+    file_name = os.path.join(par_set['tmp_dir'], ahora_seq_name('row_%d_'%(row_number), '.txt'))
+    with open(file_name, 'wb') as file_handle:
+        Z_arr.dump(file_handle)
+
+
+def write_row(complex_frame, list_tuple, par_set, row_number):
+    """  
+    """
+    left_style = np.linspace(complex_frame['upper_left'], complex_frame['bottom_left'], par_set['n_rows'])
+    right_style = np.linspace(complex_frame['upper_right'], complex_frame['bottom_right'], par_set['n_rows'])
+    #row_array = np.array(np.zeros((1, par_set['n_cols'])), dtype=complex)
+    row_array = np.linspace(left_style[row_number], right_style[row_number], par_set['n_cols'])
+
+    it_max = par_set['it_max']
+    max_d = par_set['max_d']
+    Z_arr = np.zeros((3,row_array.size),complex)
+    col = 0
+    for Z0 in row_array:
+        Z_arr[0, col] = Z0
+        Z_arr[1, col], Z_arr[2, col] = tuplerator(list_tuple, Z0, it_max, max_d)
+        col += 1
+    Z_arr[1, :] = Z_arr[1, :] + complex(0.0, float(row_number))
+    file_name = os.path.join(par_set['tmp_dir'], ahora_seq_name('row_%d_'%(row_number), '.txt'))
+    with open(file_name, 'wb') as file_handle:
+        Z_arr.dump(file_handle)
+
+        
+def tuplerator(list_tuple, Z0, it_max, max_d):
+    """ Usage: ET, Z = tuplerator(list_tuple, Z0, it_max, max_d)
+        function iterator for two tuple list (with at least one tuple)
+        list_tuple = [(function_1, (p1, p2,... , pn)), (function_2, (p1, p2,... , pn))]
+        
+    Args:
+        list_tuple: [ ( function_handle_01,(fh_01_p1, fh_01_p2,...) ),... (fh_n,(p1, p2,...)) ]
+                    list of tuples: function handle (not lambda fcn) & internal parameters
+        Z0:         complex vector - starting point
+        it_max:     maximum number of iterations
+        max_d:      quit iterating and return distance
+        
+    Returns:
+        ET:         number of iterations to finishing point
+        Z:          complex vector - finishing point
+        
+    Prototype:
+        def jsV(Z, P):
+            Z = Z**P[0] - P[1]
+            return Z
+    """
+    ET = 0
+    Z = Z0
+    d = 0
+    while (ET <= it_max) & (np.isfinite(d)) & (d < max_d):
+        ET += 1
+        Z_was = Z
+        try:
+            for fcn_hndl, P in list_tuple:
+                Z = fcn_hndl(Z, P)
+            d = np.abs(Z - Z0)
+        except:
+            return max(1, ET - 1), Z_was
+        
+    if (ET >= it_max) & (np.isfinite(d)) & (d < max_d):
+        return ET, Z
+    else:
+        return max(1, ET - 1), Z_was
+    
+    
+def ahora_seq_name(prefi_str=None, suffi_str=None):
+    """ locally unique integer time stamp name (1/1e12 sec)
+    
+    Args:
+        n_digits:  number of digits accuracy in the decimal seconds part of the name 
+        prefi_str: prepend to time_stamp
+        suffi_str: postpent to time stamp
+        
+    Returns:
+        now_name:  a time stamped file name with prefix and suffix as input 
+    """
+    time_decimal_shift = 1e12
+    ahora_nombre = '%d'%(time.time() * time_decimal_shift)
+    if prefi_str is None: prefi_str = ''
+    if suffi_str is None: suffi_str = ''
+    return prefi_str + ahora_nombre + suffi_str
+
+
+def int_to_alpha(N):
+    """ convert integer to spreadsheet column lettering
+
+    Args:    an integer representation of the letters like 136964
+
+    Returns: a string of capital letters like 'GTOV'
+    """
+    B = 26
+    ALPH = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    max_mag = 100
+    M = 0
+
+    while (N > B ** M) & (M <= max_mag):
+        M += 1
+
+    S = []
+    for nada in range(0, M):
+        M -= 1
+        D = np.int_(np.floor(N / B ** M))
+        N = N - D * B ** M
+        S.append(ALPH[D - 1])
+
+    alpha_N = ''.join(S)
+
+    return alpha_N
+
+def seq_time_id_string(time_units=1e6):
+    """ current time in time_units as an integer encoded in a base 26 caps string
+
+    Args:
+        time_units:  1e6 is micro seconds, 1 <= time_units <= 1e9
+
+    Returns:
+        tid_str:     current time in time_units as an integer encoded in a base 26 caps string
+    """
+    return int_to_alpha(int(time.time() * np.maximum(np.minimum(time_units, 1e9), 1)))
+
+
+
+def get_tmp_dir(dir_path, dir_name, timestamp=None):
+    """ create a "dir_name" with time stamp directory
+
+    Args:
+        dir_name: an existing directory such as the run directory.
+        timestamp: optional - if not input a microsecond stamp will be added.
+    Returns:
+        new_dir_name:
+    """
+    if timestamp is None:
+        timestamp = seq_time_id_string()
+    new_dir_name = os.path.join(dir_path, dir_name + timestamp)
+    os.mkdir(new_dir_name)
+
+    return new_dir_name
+
+
+def remove_tmp_dir(dir_name):
+    """ remove directory and all the files it contains.
+
+    Args:
+        dir_name: name of a directory with no sub-directories.
+    """
+    try:
+        dir_list = os.listdir(dir_name)
+        if len(dir_list) > 0:
+            for file_name in dir_list:
+                os.remove(os.path.join(dir_name, file_name))
+
+        os.rmdir(dir_name)
+    except:
+        print('function remove_tmp_dir:\n%s\n(directory DNE)' % (dir_name))
+
+    return
