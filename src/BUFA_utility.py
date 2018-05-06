0a22e0b686323a449dffc2d888d3a629ff31c5a0
dlanier <lanier4@illinois.edu>
2018-05-06 13:34:03 -0500

    more complete with formatting

--- src/BUFA_utility.py
+++ src/BUFA_utility.py
@@ -33,35 +33,45 @@ def display_module(imported_module, show_imported_functions=False):
     """
     ignore_functions_list = ['getmembers', 'isfunction', 'getsource', 'signature', 'isclass']
     
-    #                                      display classes in the module
+    #                                                            display classes in the module
     class_list = [o for o in getmembers(imported_module) if isclass(o[1])]
-    class_source_list = [getsource(o[1]) for o in getmembers(imported_module) if isclass(o[1])]
-    
-    if len(class_source_list) != len(class_list): return  #       This should not be possible
-    
-    for list_number in range(len(class_list)):
-        class_tuple = class_list[list_number]    
-        source_str = class_source_list[list_number]
-        docs_string = None
+    if len(class_list) >= 1:
+        print('\n\t\tClasses in module %s\n'%(imported_module.__name__))
+        
+    for class_list_number in range(len(class_list)):
+        selected_class = class_list[class_list_number][1]
+        class_members = getmembers(selected_class)
+        class_methods = [o for o in class_members if isfunction(o[1])]
+        print('\nclass %s\n'%(class_list[class_list_number][0]))
         try:
-            docs_string = source_str.split('"""')[1]
+            selected_class_source = getsource(selected_class)
+            docs_string = selected_class_source.split('"""')[1]
+            if docs_string is not None and len(docs_string) > 0:
+                print(docs_string, '\n')
         except:
             pass
-
-        print('class %s'%(class_tuple[0]))
-        if docs_string is None:
-            print('doc_missing\n')
-        else:
-            print(docs_string,'\n')
-            
-    #                                      display functions in the module
+        
+        display_methods_functions_list(class_methods, string_prefix='\t')
+                
+    #                                                            display functions in the module
     functions_list = [o for o in getmembers(imported_module) if isfunction(o[1])]
-    source_list = [getsource(o[1]) for o in getmembers(imported_module) if isfunction(o[1])]    
+    if len(functions_list) >= 1:
+        print('\n\t\tfunctions in module %s\n'%(imported_module.__name__))
+
+    display_methods_functions_list(functions_list, 
+                                   string_prefix='', 
+                                   ignore_functions_list=ignore_functions_list,
+                                   show_imported_functions=True)
+                
+def display_methods_functions_list(meth_func_list, 
+                                   string_prefix='',
+                                   ignore_functions_list=[],
+                                   show_imported_functions=False):
+    source_list = [getsource(o[1]) for o in meth_func_list if isfunction(o[1])]
+    if len(source_list) != len(meth_func_list): return  #       This should not be possible
     
-    if len(source_list) != len(functions_list): return  #       This should not be possible
-    
-    for list_number in range(len(functions_list)):
-        function_tuple = functions_list[list_number]    
+    for list_number in range(len(meth_func_list)):
+        function_tuple = meth_func_list[list_number]    
         if function_tuple[0] in ignore_functions_list:
             if show_imported_functions == True:
                 print('using: %s%s'%(function_tuple[0], signature(function_tuple[1])))
@@ -73,7 +83,7 @@ def display_module(imported_module, show_imported_functions=False):
             except:
                 pass
 
-            print('def %s%s'%(function_tuple[0], signature(function_tuple[1])))
+            print(string_prefix, 'def %s%s'%(function_tuple[0], signature(function_tuple[1])))
             if docs_string is None:
                 print('doc_missing\n')
             else:
