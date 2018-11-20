57ab251b6166d77c58a47cf7e730dff01ccadf3a
dlanier <lanier4@illinois.edu>
2018-11-20 09:46:26 -0600

    plot complex added

--- pyreimpic/zexhibit.py
+++ pyreimpic/zexhibit.py
@@ -6,6 +6,8 @@ mradmstr514226508@gmail.com
 functionality to display & print complex plane
 """
 import numpy as np
+import matplotlib.pyplot as plt
+
 # print_order controls display of frame in complex_frame_dict_to_string
 print_order = ['upper_left', 'top_center', 'upper_right',
                'left_center', 'center_point', 'right_center',
@@ -100,4 +102,65 @@ def complex_frame_dict_to_string(frame_dict, N_DEC=4):
 
 # plot_Z1_Z2()
 # plot_polar()
+
+
+def pyplot_raw_complex(Z, title='Z complex', ylabel='Imaginary', xlabel='Real'):
+    """ fig, ax = pyplot_raw_complex(Z, title, ylabel, xlabel)
+    plot complex vectors from origin """
+    fig = plt.figure()
+    ax = plt.axes()
+
+    if isinstance(Z, np.ndarray) or isinstance(Z, list):
+        Z_arr = mat2arr(Z)
+        for z_ix in range(Z_arr.shape[0]):
+            plt.plot([0, Z_arr[z_ix].real],
+                     [0, Z_arr[z_ix].imag],
+                     'go-', label='python')
+        limit = np.max(np.ceil(np.absolute(Z_arr)))
+
+    else:
+        plt.plot([0, Z.real], [0, Z.imag], 'bo-', label='python')
+        limit = np.max(np.ceil(np.absolute(Z)))
+
+    plt.xlim((-limit, limit))
+    plt.ylim((-limit, limit))
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    plt.title(title)
+
+    return fig, ax
+
+
+def mat2arr(M):
+    """ unroll a tensor to an array for sequential plotting etc. -- copy in to out """
+    if len(M.shape) > 1:
+        A = (M + 0.0).ravel()
+    else:
+        A = M + 0.0
+    return A
+
+
+def pyplot_on_grid_complex(Z0, Z1, title='Z complex', ylabel='height', xlabel='width'):
+    """ fig, ax = pyplot_grid_complex(Z0, Z1,title, ylabel, xlabel) """
+    Z_start = mat2arr(Z0)
+    Z_end = mat2arr(Z1)
+    if not isinstance(Z_start, np.ndarray) or isinstance(Z_start, list):
+        fig, ax = pyplot_raw_complex(Z, title, ylabel, xlabel)
+    else:
+        fig = plt.figure()
+        ax = plt.axes()
+        for arr_idx in range(Z_start.shape[0]):
+            plt.plot([Z_start[arr_idx].real, Z_end[arr_idx].real],
+                     [Z_start[arr_idx].imag, Z_end[arr_idx].imag],
+                     'bo-')
+
+    limit = max(np.max(np.ceil(np.absolute(Z_start))), np.max(np.ceil(np.absolute(Z_end))))
+
+    plt.xlim((-limit, limit))
+    plt.ylim((-limit, limit))
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    plt.title(title)
+
+    return fig, ax
 
