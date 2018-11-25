59ae69727eeb3b2fb00dcf6ec5fad6744bf7be94
dlanier <lanier4@illinois.edu>
2018-11-25 12:01:30 -0600

    class for image-name function data

--- pyreimpic/zexhibit.py
+++ pyreimpic/zexhibit.py
@@ -7,11 +7,13 @@ functionality to display & print complex plane
 """
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 # print_order controls display of frame in complex_frame_dict_to_string
 print_order = ['upper_left', 'top_center', 'upper_right',
                'left_center', 'center_point', 'right_center',
               'bottom_left', 'bottom_center', 'bottom_right']
+
 
 def complex_to_string(z, N_DEC=6):
     """ format single complex number to string with n decimal places """
@@ -144,7 +146,7 @@ def pyplot_on_grid_complex(Z0, Z1, title='Z complex', ylabel='height', xlabel='w
     Z_start = mat2arr(Z0)
     Z_end = mat2arr(Z1)
     if not isinstance(Z_start, np.ndarray) or isinstance(Z_start, list):
-        fig, ax = pyplot_raw_complex(Z, title, ylabel, xlabel)
+        fig, ax = pyplot_raw_complex(Z_end, title, ylabel, xlabel)
     else:
         fig = plt.figure()
         ax = plt.axes()
@@ -163,4 +165,52 @@ def pyplot_on_grid_complex(Z0, Z1, title='Z complex', ylabel='height', xlabel='w
 
     return fig, ax
 
+
+def toady_string_to_list(toady_string):
+    toady_string = toady_string.strip('[]').split(',')
+    for tsidx in range(len(toady_string)):
+        toady_string[tsidx] = toady_string[tsidx].replace("\'", ' ').strip().strip(';')
+    return toady_string
+
+
+class Function_Finder:
+
+    def __init__(self, images_df_name, functions_df_name):
+        self.images_df = pd.read_csv(images_df_name, sep='\t')
+        self.functions_df = pd.read_csv(functions_df_name, sep='\t')
+
+    def get_image_data(self, image_name):
+        return self.images_df[self.images_df['image_filename'] == image_name].iloc[0].to_dict()
+
+    def get_function_data(self, function_name):
+        return self.functions_df[self.functions_df['equation_name'] == function_name].iloc[0].to_dict()
+
+    def get_image_function_data(self, image_name):
+        image_dict = self.get_image_data(image_name)
+        function_dict = self.get_function_data(image_dict['function_name'][:-2])
+        return image_dict, function_dict
+
+    def display_function_for_image(self, image_name):
+        image_dict, function_dict = self.get_image_function_data(image_name)
+        print('Matlab for', '%30s \tfrom \t%s\n' % (image_name, image_dict['function_name']))
+        print('parameters')
+        for s in toady_string_to_list(image_dict['parameters']):
+            print('%30s' % (s))
+        print('control parameters')
+        print('%25s: %s' % ('max_iter', image_dict['max_iter']))
+        print('%25s: %s' % ('max_dist', image_dict['max_dist']))
+
+        print('internal vars')
+        for s in toady_string_to_list(function_dict['internal_vars']):
+            print('%30s' % (s))
+        print('ET Loop')
+        indent_str = ' ' * 4
+        for s in toady_string_to_list(function_dict['while_lines']):
+            if s[0:5] == 'while':
+                print(indent_str, s.replace('\\n', ''))
+            elif s[0:3] == 'for':
+                print(indent_str, ' ' * 2, s.replace('\\n', ''))
+            elif not s[0:3] == 'end':
+                print(indent_str, ' ' * 4, s.replace('\\n', ''))
+        print('')
 
